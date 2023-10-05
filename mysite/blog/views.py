d30ccf570d0e92bc16b6f1f7d41c21d6f12612a1
Jungsik <sk7556@naver.com>
2023-10-05 23:52:28 +0900

    1005

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def blog(request):
+
+    return render(request, 'blog/blog.html')    
+
+def post(request, pk):
+
+    return render(request, 'blog/post.html')
