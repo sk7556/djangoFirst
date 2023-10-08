05f062700268f393a3e4369c52832c585e22c416
Jungsik <sk7556@naver.com>
2023-10-08 17:55:46 +0900

    22

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'), # accounts/login   
+    path('login/', views.login, name='login'), # accounts/login      
     path('logout/', views.logout, name='logout'), # accounts/logout
 ]
