087169154bcc635ea3cf54db4d81292ac970342f
Jungsik <sk7556@naver.com>
2023-10-17 15:52:27 +0900

    1017

--- mysite/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'), # accounts/login      
-    path('logout/', views.logout, name='logout'), # accounts/logout
-]
--- mysite/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def blog(request):
-
-    return render(request, 'blog/blog.html')    
-
-def post(request, pk):
-
-    return render(request, 'blog/post.html')
